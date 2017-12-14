1d2659d27d192dc512969eadb878dc3ab8ac53c4
Taylor Huang <34359329+TaylorHuang2017@users.noreply.github.com>
2017-12-14 10:19:52 +0800

    first edition

---
+++ JDSignup.py
@@ -0,0 +1,18 @@
+import time
+from selenium import webdriver
+username = "yourusername"
+password = "yourpassword"
+
+driver=webdriver.Chrome()
+driver.get('http://vip.jd.com')
+time.sleep(3)
+
+
+driver.find_element_by_link_text('账户登录').click()
+driver.find_element_by_id('loginname').click()
+driver.find_element_by_id('loginname').send_keys(username)
+driver.find_element_by_id('nloginpwd').click()
+driver.find_element_by_id('nloginpwd').send_keys(password)
+driver.find_element_by_id('loginsubmit').click()
+time.sleep(1)
+driver.find_element_by_class_name('icon-sign').click()
