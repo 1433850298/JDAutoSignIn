22efb97f09048100a07cef8a6531edcb7fe7bd14
Taylor Huang <34359329+TaylorHuang2017@users.noreply.github.com>
2017-12-28 20:15:53 +0800

    Update JDSignup.py

--- JDSignup.py
+++ JDSignup.py
@@ -1,3 +1,4 @@
+# written by Taylor Huang 
 import time
 from selenium import webdriver
 username = "yourusername"
@@ -5,7 +6,6 @@ password = "yourpassword"
 
 driver=webdriver.Chrome()
 driver.get('http://vip.jd.com')
-time.sleep(1)
 
 
 driver.find_element_by_link_text('账户登录').click()
@@ -15,6 +15,16 @@ driver.find_element_by_id('nloginpwd').click()
 driver.find_element_by_id('nloginpwd').send_keys(password)
 driver.find_element_by_id('loginsubmit').click()
 time.sleep(1)
-driver.find_element_by_id('signIn').click()
 
-driver.close()
+try:
+    driver.find_element_by_id('signIn').click()  #签到领京豆
+    print("签到成功！")
+    time.sleep(1)
+    driver.find_element_by_class_name('gift').click()  #领取礼包
+    driver.get("http://datawallet.jd.com/profile.html")
+    driver.find_element_by_class_name('btn-sign').click()  # 签到领流量
+    print("领取流量成功！")
+    driver.quit()  #若签到成功，关闭浏览器并安静退出
+except:
+    print("签到失败，请检查.")  #若签到失败，打印错误信息
+    driver.close()  #关闭浏览器，但保留命令行，提示用户检查错误的原因
